c16b7a6d0c499530e6c02ece203a3b863938f9c5
Xaviermello <contatoshenriquexavier@gmail.com>
2026-03-10 11:01:50 -0300

    First commit

---
+++ src/test/java/com/xaviervinicius/labschedule/LabScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xaviervinicius.labschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
